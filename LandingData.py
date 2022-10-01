591285dd82cc923fcd63d1c7d69253b94b69945c
matt-gree <m10greene@gmail.com>
2022-10-01 18:39:38 -0500

    Add Character Summary
    Also created Export List to Excel Module

--- LandingData.py
+++ LandingData.py
@@ -7,8 +7,8 @@ import RioStatsConverter
 import MattGreeRioWebExtensions as RWext
 
 import openpyxl
-from openpyxl import load_workbook
-from openpyxl.worksheet.table import Table, TableStyleInfo
+
+import Export_List_To_Excel
 
 print("Enter '0' for stars-off data; '1' for stars-on")
 stars = int(input())
@@ -88,41 +88,7 @@ for event in jsonObj["Data"]:
                         MattGreeRioWebExtensions.stat_lookup(event["pitcher_char_id"],"0x2")
                         ])
 
+Export_List_To_Excel.export_to_excel(export_list, filepath)
 
-def export_to_excel(stats, excel_file):
-    wb = load_workbook(excel_file)
-    sheets = wb.sheetnames
-    ws1 = wb[sheets[0]]
-
-    try:
-        del ws1.tables["MarioBaseballStats"]
-    except KeyError:
-        table = True
-
-    wb.save(excel_file)
-
-    ws1.delete_rows(1, ws1.max_row)  # clears out old values in the excel sheet
-    wb.save(excel_file)
-
-    for row in stats:
-        ws1.append(row)
-    wb.save(excel_file)
-
-    def column_string(n):
-        string = ""
-        while n > 0:
-            n, remainder = divmod(n - 1, 26)
-            string = chr(65 + remainder) + string
-        return string
-
-    table_size = "A1:" + str(column_string(len(export_list[0]))) + str(len(export_list))
-    tab = Table(displayName="MarioBaseballStats", ref=table_size)
-    style = TableStyleInfo(name="TableStyleMedium9", showFirstColumn=False, showLastColumn=False, showRowStripes=True, showColumnStripes=True)
-    tab.tableStyleInfo = style
-    ws1.add_table(tab)
-    wb.save(excel_file)
-
-
-export_to_excel(export_list, filepath)
 print("Complete")
 
